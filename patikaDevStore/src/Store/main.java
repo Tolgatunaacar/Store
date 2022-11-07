23d6301d75fde34e78b1995831147eeb28881833
Tolga Tuna Acar <110130709+Tolgatunaacar@users.noreply.github.com>
2022-11-08 01:19:19 +0300

    Add files via upload

---
+++ patikaDevStore/src/Store/main.java
@@ -0,0 +1,15 @@
+package Store;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class main {
+
+
+         public static void main(String[] args) {
+             StoreFunctions patikaStore = new StoreFunctions();
+             patikaStore.run();
+         }
+
+}
